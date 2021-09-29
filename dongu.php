5eaafe63cff003483d87424a04f8a1dfa95f2fbd
=abdurrahim <=abdurrahim58eker@gmail.com>
2021-09-29 11:04:12 +0300

    döngüler

---
+++ dongu.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" href="sekil.css">
+</head>
+<body>
+	<div class="birinci">
+	<?php
+	
+	for ($c=1; $c <3; $c++) { 	
+	for ($i=1; $i <21; $i++) { 
+	//$i'nin değerini 1 verdik. $i'nin değeri 11'den küçükse $i'nin değerini 1 artır.	
+			for ($x=1; $x < $i ; $x++) { 
+				//$x'nin değerini 1 verdik. $x $i'den küçükse $x'in değerini 1 artır.
+				echo $x;//$x in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}
+	for ($a=21; $a > 1; $a--) { 
+		//$a'nin değerini 11 verdik. $a'nin değeri 1'den büyükse $a'nin değerini 1 azalt.
+			for ($b=1; $b < $a ; $b++) { 
+				//$b'nin değerini 1 verdik. $b $a'dan küçükse $b'in değerini 1 artır.
+				echo $b;
+				//$b in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}	
+	echo $c;
+	}
+		
+
+?> 
+</div>
+<div class="ikinci">
+	<?php
+	
+	for ($c=1; $c <3; $c++) { 	
+	for ($i=21; $i >1; $i--) { 
+	//$i'nin değerini 1 verdik. $i'nin değeri 11'den küçükse $i'nin değerini 1 artır.	
+			for ($x=1; $x < $i ; $x++) { 
+				//$x'nin değerini 1 verdik. $x $i'den küçükse $x'in değerini 1 artır.
+				echo $x;//$x in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}
+	for ($a=1; $a < 21; $a++) { 
+		//$a'nin değerini 11 verdik. $a'nin değeri 1'den büyükse $a'nin değerini 1 azalt.
+			for ($b=1; $b < $a ; $b++) { 
+				//$b'nin değerini 1 verdik. $b $a'dan küçükse $b'in değerini 1 artır.
+				echo $b;
+				//$b in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}	
+	echo $c;
+	}
+		
+
+?>
+	</div>
+	<div class="ucuncu">
+	<?php
+	
+	for ($c=1; $c <3; $c++) { 	
+	for ($i=21; $i >1; $i--) { 
+	//$i'nin değerini 1 verdik. $i'nin değeri 11'den küçükse $i'nin değerini 1 artır.	
+			for ($x=1; $x < $i ; $x++) { 
+				//$x'nin değerini 1 verdik. $x $i'den küçükse $x'in değerini 1 artır.
+				echo $x;//$x in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}
+	for ($a=1; $a < 21; $a++) { 
+		//$a'nin değerini 11 verdik. $a'nin değeri 1'den büyükse $a'nin değerini 1 azalt.
+			for ($b=1; $b < $a ; $b++) { 
+				//$b'nin değerini 1 verdik. $b $a'dan küçükse $b'in değerini 1 artır.
+				echo $b;
+				//$b in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}	
+	echo $c;
+	}
+		
+
+?>
+	</div>
+		<div class="dorduncu">
+	<?php
+	
+	for ($c=1; $c <3; $c++) { 	
+	for ($i=1; $i <22; $i++) { 
+	//$i'nin değerini 1 verdik. $i'nin değeri 11'den küçükse $i'nin değerini 1 artır.	
+			for ($x=1; $x < $i ; $x++) { 
+				//$x'nin değerini 1 verdik. $x $i'den küçükse $x'in değerini 1 artır.
+				echo $x;//$x in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}
+	for ($a=21; $a > 1; $a--) { 
+		//$a'nin değerini 11 verdik. $a'nin değeri 1'den büyükse $a'nin değerini 1 azalt.
+			for ($b=1; $b < $a ; $b++) { 
+				//$b'nin değerini 1 verdik. $b $a'dan küçükse $b'in değerini 1 artır.
+				echo $b;
+				//$b in degerini ekrana yaz.
+			}
+							echo "</br>";//al satıra geç.
+		}	
+	echo $c;
+	}
+		
+
+?> 
+</div>
+</body>
+</html>
